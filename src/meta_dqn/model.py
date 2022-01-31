30ba6d0219629fa212cfd66602c311876d069cfe
Anil Zeybek <anil.zeybek98@gmail.com>
2022-02-01 01:31:10 +0300

    change sizes to dims in meta_dqn model

--- src/meta_dqn/model.py
+++ src/meta_dqn/model.py
@@ -7,17 +7,17 @@ class QNetwork(nn.Module):
     def __init__(self, obs_dim: int, action_dim: int, hidden_1: int, hidden_2: int) -> None:
         super(QNetwork, self).__init__()
 
-        self.obs_size = obs_dim
-        self.action_size = action_dim
+        self.obs_dim = obs_dim
+        self.action_dim = action_dim
         self.hidden_1 = hidden_1
         self.hidden_2 = hidden_2
 
-        self.fc1 = nn.Linear(self.obs_size, self.hidden_1)
+        self.fc1 = nn.Linear(self.obs_dim, self.hidden_1)
         self.fc2 = nn.Linear(self.hidden_1, self.hidden_2)
-        self.fc3 = nn.Linear(self.hidden_2, self.action_size)
+        self.fc3 = nn.Linear(self.hidden_2, self.action_dim)
 
-    def change_last_layer(self, new_size: int) -> None:
-        self.fc3 = nn.Linear(self.hidden_2, new_size)
+    def change_last_layer(self, new_dim: int) -> None:
+        self.fc3 = nn.Linear(self.hidden_2, new_dim)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = F.relu(self.fc1(x))
